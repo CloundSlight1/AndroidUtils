cec072ced2b26c937c8b5e8dffac16a5611a13f4
wuyuzhong <wuyuz@landicorp.com>
2017-01-03 15:43:28 +0800

    add okhttp

--- app/src/main/java/com/wuyz/androidutils/view/MainActivity.java
+++ app/src/main/java/com/wuyz/androidutils/view/MainActivity.java
@@ -6,8 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
+import com.wuyz.androidutils.Log2;
 import com.wuyz.androidutils.R;
 import com.wuyz.androidutils.manager.ViewUtils;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by wuyz on 2016/9/29.
@@ -16,14 +26,14 @@ import com.wuyz.androidutils.manager.ViewUtils;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ViewUtils.setStatusBarTransparent(this);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-
-        findViewById(R.id.recyclerView).setOnClickListener(this);
     }
 
     @Override
@@ -31,6 +41,25 @@ public class MainActivity extends Activity implements View.OnClickListener {
         switch (v.getId()) {
             case R.id.recyclerView:
                 startActivity(new Intent(this, RecyclerViewActivity.class));
+                break;
+            case R.id.okHttp:
+                Request.Builder builder = new Request.Builder();
+                builder.get().url("http://www.codeceo.com/");
+                Request request = builder.build();
+                OkHttpClient client = new OkHttpClient.Builder().connectTimeout(5, TimeUnit.SECONDS)
+                        .readTimeout(5, TimeUnit.SECONDS).build();
+                Call call = client.newCall(request);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        Log2.e(TAG, e);
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        Log2.d(TAG, "onResponse %s", response.body().string());
+                    }
+                });
                 break;
         }
     }
